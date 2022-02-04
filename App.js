dff05845931e77830ef2fa8790b61e012cd52406
belchushka <savvashu@gmail.com>
2022-02-05 00:22:07 +0300

    ..

--- App.js
+++ App.js
@@ -1,18 +1,42 @@
-import {AsyncStorage, LogBox, TextInput} from 'react-native';
+import {AsyncStorage, LogBox, Platform, TextInput, View, StyleSheet} from 'react-native';
 import {SafeAreaView} from "react-native-safe-area-context";
 import {Provider} from "react-redux";
 import store from "./src/store";
 import NavigationComponent from "./src/components/NavigationComponent";
+import {StatusBar} from "expo-status-bar";
 TextInput.defaultProps.selectionColor = '#777777'
 LogBox.ignoreAllLogs();
+
+const MyStatusBar = ({backgroundColor, ...props}) => (
+    <View style={[styles.statusBar, { backgroundColor }]}>
+        <SafeAreaView>
+            <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+        </SafeAreaView>
+    </View>
+);
 export default function App() {
     return (
         <Provider store={store}>
             <SafeAreaView style={{flex: 1}}>
+                <MyStatusBar backgroundColor="#f2f2f2" barStyle="light-content" />
                 <NavigationComponent />
             </SafeAreaView>
         </Provider>
 
     );
 }
+const STATUSBAR_HEIGHT = StatusBar.currentHeight;
+const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    statusBar: {
+        height: STATUSBAR_HEIGHT,
+    },
+    content: {
+        flex: 1,
+        backgroundColor: '#33373B',
+    },
+});
 
